b10fee34d3b39a773c94db8ce438a70c4002de36
Jinchao Liu <chashanchao@users.noreply.github.com>
2016-10-31 11:32:18 +0000

    Create softmargins_adaboost.py

---
+++ examples/softmargins_adaboost.py
@@ -0,0 +1,6 @@
+"""Implement the methods in the following paper 
+
+    G. Ratsch, T. Onoda, and K. R. Muller, 'Soft margins for AdaBoost', Machine Learning, 
+    vol.42, pp. 287-320, March 2001. 
+"""
+
